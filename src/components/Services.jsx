3042155fff62f660e074473d7bc4dab45313433d
pcf26536 <muffwaindan@gmail.com>
2020-02-04 12:08:48 +0300

    update discount style [ch32]

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   return (
     <div id={'services'} className={'services-modal hidden'}>
       <h4>Our Services</h4>
-      {services.map((service) => (<a href={service.link} className={'service-link'}>{service.title}</a>))}
+      {services.map((service) => (<a href={service.link} className={'service-link'} key={service.key}>{service.title}</a>))}
       <p className={'info'}>click anywhere outside this dialog to close it</p>
     </div>
   );
