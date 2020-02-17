85208dba0c81b659bb34652c66ff27e2478a9e8e
pcf26536 <muffwaindan@gmail.com>
2020-02-17 12:16:32 +0300

    update menu links [ch32]

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -1,12 +1,14 @@
 const signUp = 'Sign Up';
 const menuTitle = 'Menu';
 const menuItems = ['Home', 'Payment', 'Become a Tutor', 'About Us', 'Trending', 'FAQs'];
+const menuLinks = ['https://rabbii.co.ke', 'https://tutor.rabbii.co.ke', 'https://about.rabbii.co.ke',
+  'https://trending.rabbii.co.ke', 'https://faqs.rabbii.co.ke'];
 const titles = ['Types of teachers', 'tuition types', 'get a discount'];
 const hidden = 'hidden';
 
 const texts = {
   signUp: signUp, menuTitle: menuTitle,
-  menuItems: menuItems, hidden: hidden, titles: titles
+  menuItems: menuItems, hidden: hidden, titles: titles, menuLinks: menuLinks
 };
 
 export default texts;
